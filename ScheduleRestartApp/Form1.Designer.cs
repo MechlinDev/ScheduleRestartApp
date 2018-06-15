b879555fd6857a7fc97a293efcd9b10d7aa89239
Sudhir Mittal <sudhir.mittal@mechlintech.com>
2018-06-15 21:53:37 +0530

    new 15 june 2018

--- ScheduleRestartApp/Form1.Designer.cs
+++ ScheduleRestartApp/Form1.Designer.cs
@@ -28,39 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.chkAutoLogOn = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.LblDescription = new System.Windows.Forms.Label();
+            this.lblDomain = new System.Windows.Forms.Label();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // chkAutoLogOn
+            // Title
             // 
-            this.chkAutoLogOn.AutoSize = true;
-            this.chkAutoLogOn.Location = new System.Drawing.Point(12, 39);
-            this.chkAutoLogOn.Name = "chkAutoLogOn";
-            this.chkAutoLogOn.Size = new System.Drawing.Size(161, 17);
-            this.chkAutoLogOn.TabIndex = 0;
-            this.chkAutoLogOn.Text = "Auto Logon For Current User";
-            this.chkAutoLogOn.UseVisualStyleBackColor = true;
-            this.chkAutoLogOn.CheckedChanged += new System.EventHandler(this.chkAutoLogOn_CheckedChanged);
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.SystemColors.Control;
+            this.Title.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Title.Location = new System.Drawing.Point(193, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(133, 24);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "Smart Login";
             // 
-            // label1
+            // LblDescription
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.Control;
-            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.label1.Location = new System.Drawing.Point(193, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Scheduled Restart Application";
+            this.LblDescription.AutoSize = true;
+            this.LblDescription.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDescription.Location = new System.Drawing.Point(31, 48);
+            this.LblDescription.Name = "LblDescription";
+            this.LblDescription.Size = new System.Drawing.Size(486, 16);
+            this.LblDescription.TabIndex = 2;
+            this.LblDescription.Text = "To set Auto Logon and create restart task, privide domain username and password.";
+            // 
+            // lblDomain
+            // 
+            this.lblDomain.AutoSize = true;
+            this.lblDomain.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDomain.Location = new System.Drawing.Point(41, 84);
+            this.lblDomain.Name = "lblDomain";
+            this.lblDomain.Size = new System.Drawing.Size(42, 14);
+            this.lblDomain.TabIndex = 3;
+            this.lblDomain.Text = "Domain";
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDomain.Location = new System.Drawing.Point(172, 84);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.Size = new System.Drawing.Size(248, 20);
+            this.txtDomain.TabIndex = 4;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(41, 124);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(56, 14);
+            this.lblUser.TabIndex = 5;
+            this.lblUser.Text = "Username";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(172, 124);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(248, 20);
+            this.txtUser.TabIndex = 6;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoEllipsis = true;
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(41, 165);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(57, 14);
+            this.lblPass.TabIndex = 7;
+            this.lblPass.Text = "Password";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(172, 165);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(248, 20);
+            this.txtPass.TabIndex = 8;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(227, 214);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(99, 23);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.ClientSize = new System.Drawing.Size(552, 278);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.chkAutoLogOn);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.txtDomain);
+            this.Controls.Add(this.lblDomain);
+            this.Controls.Add(this.LblDescription);
+            this.Controls.Add(this.Title);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -70,9 +152,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.CheckBox chkAutoLogOn;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label LblDescription;
+        private System.Windows.Forms.Label lblDomain;
+        private System.Windows.Forms.TextBox txtDomain;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
 
--- ScheduleRestartApp/Form1.cs
+++ ScheduleRestartApp/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Security.Principal;
@@ -18,46 +20,109 @@ namespace ScheduleRestartApp
         {
             InitializeComponent();
         }
-
+        string RegistryLocation = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon";
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-        }
+            //            Application will check for autologon enabled if it is enabled then application exists. 
+            //Otherwise it runs AutoLogon -sysinternal app(both apps need to be placed in the same directory with files for task scheduler) which asks user for password.
+            //And it recursively waits for 60 seconds and then rechecks if the autologon is enabled.
+            //If autologon is enabled then it adds a task to task scheduler to restart the system at 2 AM with autologon account and adds a shortcut to run this app on windows startup and rechecks all the steps.
 
-        private void chkAutoLogOn_CheckedChanged(object sender, EventArgs e)
-        {
-            if (chkAutoLogOn.Checked)
-            {
-                DialogResult result = MessageBox.Show("Are you sure to use current Logon?", "Confirm!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            //            Kamaraj...This application is not doing what I asked for.  A couple of things that are important.I can't distribute the Sysinternals application so that is not going to work.  I need an application that opens a form that I can design and edit.  The form needs to ask for the password and then handles the tasks.  The form would look something along the line of the attached graphic.  The User Name could be autopopulated with the current user or it could be left off entirely.  I need it to be a self-contained application and I can't have it use external applications that I can't distribute.
+            //The image is now attached.  Also, I noticed that the application kept running every minute or so.I do not what it to do that.I can set it to run when the computer starts.I don't need this to be done by the application.
+
+            //----------------check auto  logon
+
+            //var key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            //            key.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", RegistryKeyPermissionCheck.ReadWriteSubTree);
+            //            key.SetValue("AutoAdminLogon", "1", RegistryValueKind.String);
+            //            key.SetValue("DefaultUserName", "guest", RegistryValueKind.String);
+            //            key.SetValue("DefaultPassword", "password", RegistryValueKind.String);
+
+            try
+            { 
+              
+                var key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                txtDomain.Text = (string)(key.OpenSubKey(RegistryLocation, RegistryKeyPermissionCheck.ReadWriteSubTree)).GetValue("DefaultDomainName");
+                txtUser.Text = (string)(key.OpenSubKey(RegistryLocation, RegistryKeyPermissionCheck.ReadWriteSubTree)).GetValue("DefaultUserName");
                 Application.DoEvents();
-                if (result == DialogResult.Yes)
+                string IsEnabled = (string)(key.OpenSubKey(RegistryLocation, RegistryKeyPermissionCheck.ReadWriteSubTree)).GetValue("AutoAdminLogon");
+                key.Close();
+                if (IsEnabled=="1")
                 {
-                    //yes...
-                    WindowsIdentity currUser = WindowsIdentity.GetCurrent();
-                    if (currUser != null)
-                    {
-                        string userName = currUser.Name;
-                        MessageBox.Show(userName);
-                        Application.DoEvents();
-                    }
-                    else
-                    {
-                        //ask password and loginto windows
-                    }
+                    //Do nothing and exit
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    Application.Exit();
                 }
-                else if (result == DialogResult.No)
+                else
                 {
-                    //no...
-                    MessageBox.Show("NO");
-                    Application.DoEvents();
+                    // do nothing wait for button click
                 }
             }
-            
-           
-            
-           
+            catch (Exception ex)
+            {
+                LogExceptions(ex);
+                MessageBox.Show("Application exited with error message: " + Environment.NewLine +Environment.NewLine + ex.Message, "Ooops! something went wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                Application.Exit();
+            }
+            finally
+            {
+              // do not exit here
+             
+            }
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUser.Text)|| string.IsNullOrEmpty(txtPass.Text)||string.IsNullOrEmpty(txtDomain.Text))
+            {
+                MessageBox.Show("All fields are required!", "Ooops! something went wrong",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+               // key.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                key.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", RegistryKeyPermissionCheck.ReadWriteSubTree).SetValue("AutoAdminLogon", "1", RegistryValueKind.String);
+                key.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", RegistryKeyPermissionCheck.ReadWriteSubTree).SetValue("DefaultUserName",txtUser.Text, RegistryValueKind.String);
+                key.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", RegistryKeyPermissionCheck.ReadWriteSubTree).SetValue("DefaultPassword", txtPass.Text , RegistryValueKind.String);
+                key.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", RegistryKeyPermissionCheck.ReadWriteSubTree).SetValue("DefaultDomainName", txtDomain.Text, RegistryValueKind.String);
+                key.Close();
+                MessageBox.Show("Auto-Logon Enabled Successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                Application.Exit();
+            }
+
         }
 
 
+        private static void LogExceptions(Exception e) {
+            try
+            {
+                if(!Directory.Exists(@"C:\ScheduleRestartAppLogs"))
+                    {
+                    Directory.CreateDirectory(@"C:\ScheduleRestartAppLogs");
+                }
+                using (StreamWriter sw = new StreamWriter(@"C:\ScheduleRestartAppLogs\ErrorLog_" + DateTime.Now.ToString("yyyymmddhhMMss") + ".txt"))
+                {
+                    sw.WriteLine(e.Message);
+                    sw.WriteLine(Environment.NewLine);
+                    sw.WriteLine(e.ToString());
+                    sw.Flush();
+                }
+            }
+            catch(Exception)
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                Application.Exit();
+            }
+
+        }
     }
 }
